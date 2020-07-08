e70293992ac1d28abb16ff0c631625ba1f120fe0
Mark Liu <ldtheended@163.com>
2020-07-08 18:19:51 +0800

    as0.01-2-example01-2（react的state使用更新state）

--- study-hooks/react-app/src/hooks/state.js
+++ study-hooks/react-app/src/hooks/state.js
@@ -6,12 +6,22 @@ class State extends Component{
         age:18
     }
 
+    setAge=()=>{
+        let {age} = this.state;
+        this.setState(
+            {
+                age: ++age
+            }
+        )
+    }
+
     render(){
         let {name,age} = this.state;
         return(
             <div>
                 姓名：{name},<br/>
-                年龄: {age}
+                年龄: {age},<br/>
+                <button onClick={this.setAge}>长大一岁</button>
             </div>
         );
     }
