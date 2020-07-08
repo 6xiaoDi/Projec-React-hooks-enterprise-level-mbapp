4e2910b61c72e01507bed4ca60ae6728c0121ff2
Mark Liu <ldtheended@163.com>
2020-07-08 18:34:44 +0800

    as0.01-3-example01-3（react的state使用Hooks中useState的返回值）

--- study-hooks/react-app/src/hooks/state.js
+++ study-hooks/react-app/src/hooks/state.js
@@ -1,30 +1,16 @@
-import React,{Component} from "react";
+import React,{Component,useState} from "react";
 
-class State extends Component{
-    state = {
-        name:'Leo',
-        age:18
-    }
-
-    setAge=()=>{
-        let {age} = this.state;
-        this.setState(
-            {
-                age: ++age
-            }
-        )
-    }
-
-    render(){
-        let {name,age} = this.state;
-        return(
-            <div>
-                姓名：{name},<br/>
-                年龄: {age},<br/>
-                <button onClick={this.setAge}>长大一岁</button>
-            </div>
-        );
-    }
+function State() {
+    console.log(useState({
+        name:"leo",
+        age: 18
+    }));
+    return (<div>
+        姓名:,<br />
+        年龄:,<br />
+        <button onClick={()=>{
+        }}>长大一岁</button>
+    </div>);
 }
 
 export default State;
