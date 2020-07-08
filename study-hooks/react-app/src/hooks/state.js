61db40a919486cc1e23c8865949dcf12a056adeb
Mark Liu <ldtheended@163.com>
2020-07-08 18:43:25 +0800

    as0.01-4-example01-4（react的state使用Hooks中实现数据添加和更新）

--- study-hooks/react-app/src/hooks/state.js
+++ study-hooks/react-app/src/hooks/state.js
@@ -1,14 +1,19 @@
 import React,{Component,useState} from "react";
 
 function State() {
-    console.log(useState({
+    const [state,setAge] = useState({
         name:"leo",
         age: 18
-    }));
+    });
+    let {name,age} = state;
     return (<div>
-        姓名:,<br />
-        年龄:,<br />
+        姓名:{name},<br />
+        年龄:{age},<br />
         <button onClick={()=>{
+            setAge({
+                ...state,
+                age: age + 1
+            });
         }}>长大一岁</button>
     </div>);
 }
