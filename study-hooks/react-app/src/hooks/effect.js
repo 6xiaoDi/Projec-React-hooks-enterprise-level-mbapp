cc414415fca9315dba4c3af127aa4b6a6da15af2
Mark Liu <ldtheended@163.com>
2020-07-09 11:25:26 +0800

    as0.02-7-example02-7（react的生命周期useEffect使用用函数式组件演示生命周期，缺组件即将卸载的周期）

--- study-hooks/react-app/src/hooks/effect.js
+++ study-hooks/react-app/src/hooks/effect.js
@@ -1,66 +1,43 @@
-import React,{Component,useState} from "react";
+import React, { Component, useState, useEffect } from 'react';
 
-class Txt extends Component{
-    componentWillUnmount(){
-        console.log("组件即将卸载");
-    }
-    render(){
-        let {text,setEdit} = this.props;
-        return (
-            <div>{text}<a onClick={()=>{
-                setEdit(true);
-            }}>编辑</a></div>
-        );
-    }
+function Txt(props){
+    let {text,setEdit} = props;
+    useEffect(()=>{
+        console.log("组件状态有变化了");
+    });
+    return (
+        <div>{text}<a onClick={()=>{
+            setEdit(true);
+        }}>编辑</a></div>
+    );
 }
-
-class Effect extends Component{
-   state = {
-        text: "这是今天的进度",
-        edit: false  // 是否显示编辑状态
-   }
-
-   // 更新编辑方法
-   setEditState=(edit)=>{
-        this.setState({
-            edit
-        });
-   }
-
-
-   componentDidMount(){
-       console.log("组件挂载完毕");
-   }
-
-   componentDidUpdate(){
-       console.log("组件更新完毕");
-   }
-
-   render(){
-      let {text,edit} = this.state;
-      return (<div>
-            {edit?
-                <input
-                    type="text"
-                    value={text}
-                    onChange={
-                        (e) => {
-                            this.setState({
-                            text:e.target.value
-                            })
-                        }
+function Effect(){
+    const [text,setText] = useState("这是今天的课程");
+    const [edit,setEdit] = useState(false);
+    // useEffect接收回调函数
+    useEffect(()=>{
+        console.log("状态有改变");
+    });
+    return (<div>
+        {edit?
+            <input
+                type="text"
+                value = {text}
+                onChange = {
+                    (e)=>{
+                        setText(e.target.value);
                     }
-                    onBlur={
-                        ()=>{
-                            this.setEditState(false);
-                        }
+                }
+                onBlur = {
+                    ()=>{
+                        setEdit(false)
                     }
-                />
-                :
-                <Txt text={text} setEdit={this.setEditState}/>
-            }
-      </div>);
-   }
+                }
+            />
+            :
+            <Txt text={text} setEdit={setEdit} />
+        }
+    </div>);
 }
 
 export default Effect;
