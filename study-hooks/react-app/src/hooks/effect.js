ce709217d26149baba93c3a187330807033208e0
Mark Liu <ldtheended@163.com>
2020-07-09 10:12:08 +0800

    as0.02-5-example02-5（react的生命周期useEffect使用生命周期函数组件挂载完毕、更新完毕，融入函数式组件）

--- study-hooks/react-app/src/hooks/effect.js
+++ study-hooks/react-app/src/hooks/effect.js
@@ -1,14 +1,35 @@
 import React,{Component,useState} from "react";
 
+function Txt(props){
+    let {text,setEdit} = props;
+
+    return (
+        <div>{text}<a onClick={()=>{
+            setEdit(true);
+        }}>编辑</a></div>
+    );
+}
+
 class Effect extends Component{
    state = {
         text: "这是今天的进度",
         edit: false  // 是否显示编辑状态
    }
+
+   // 更新编辑方法
    setEditState=(edit)=>{
         this.setState({
             edit
         });
+   }
+
+
+   componentDidMount(){
+       console.log("组件挂载完毕");
+   }
+
+   componentDidUpdate(){
+       console.log("组件更新完毕");
    }
 
    render(){
@@ -27,19 +48,12 @@ class Effect extends Component{
                     }
                     onBlur={
                         ()=>{
-                            this.setState({
-                                edit:false
-                            })
+                            this.setEditState(false);
                         }
                     }
                 />
                 :
-                <div >{text}  <a onClick={()=>{
-                    this.setState({
-                        edit: true
-                    })
-                }}>编辑</a>
-                </div>
+                <Txt text={text} setEdit={this.setEditState}/>
             }
       </div>);
    }
