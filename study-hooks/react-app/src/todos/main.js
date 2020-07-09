824d00fa1e8e43e2112c3fe93fab0ce3f56c36c3
Mark Liu <ldtheended@163.com>
2020-07-09 23:10:01 +0800

    as0.06-5-example06-5（实现TodoLists实现部分todos数据关联起来）

--- study-hooks/react-app/src/todos/main.js
+++ study-hooks/react-app/src/todos/main.js
@@ -1,15 +1,25 @@
 import React, {useState, useEffect, useRef } from 'react';
-function Li(){
+
+function Li(props){
+    let {inner} = props;
     return (
-        <li className="">
+        <li className={inner.completed ? "done" : ""}>
             <div className="view" >
-                <input className="toggle" type="checkbox"/>
-                <label>213213</label>
+                <input
+                    className="toggle"
+                    type="checkbox"
+                    checked={inner.completed}
+                />
+                <label>{inner.val}</label>
                 <a className="destroy">
 
                 </a>
             </div>
-            <input className="edit" type="text" value="213213"/>
+            <input
+                className="edit"
+                type="text"
+                value={inner.val}
+            />
         </li>
     )
 }
@@ -26,7 +36,14 @@ function Mian(props){
         <input id="toggle-all" type="checkbox" checked=""/>
         <label htmlFor="toggle-all">Mark all as complete</label>
         <ul id="todo-list">
-            <Li/>
+            {
+                todos.map(item=> {
+                    return <Li
+                        key = {item.id}
+                        inner = {item}
+                    />
+                })
+            }
         </ul>
     </section>
     );
