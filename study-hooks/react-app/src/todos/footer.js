cb1a255b589349496362f2271ebd720129062f5f
Mark Liu <ldtheended@163.com>
2020-07-10 11:33:12 +0800

    as0.06-12-example06-12（实现TodoLists完成footer功能）

--- study-hooks/react-app/src/todos/footer.js
+++ study-hooks/react-app/src/todos/footer.js
@@ -1,14 +1,20 @@
 import React from 'react';
 function Footer(props){
     let {todos} = props;
+    let unCompleted = todos.filter(item=>!item.completed);
+    let completed = todos.filter(item=>item.completed);
       return (
           <footer
               style={{
                   display:todos.length > 0 ? "block":"none"
               }}
           >
-              <a id="clear-completed" >Clear 0 completed item</a>
-              <div id="todo-count">10 items left</div>
+              <a id="clear-completed"
+                 style={{
+                     display:completed.length > 0?"block":"none"
+                 }}
+              >Clear {completed.length} completed item</a>
+              <div id="todo-count">{unCompleted.length} items left</div>
           </footer>
       );
 }
