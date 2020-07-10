18fe18d424b5dfcfa4f2fcd9a12acc6b140b18bb
Mark Liu <ldtheended@163.com>
2020-07-10 14:00:11 +0800

    as0.09-2-example09-2（useContext 类组件消息传递-CreateContext使用）

--- study-hooks/react-app/src/hooks/context.js
+++ study-hooks/react-app/src/hooks/context.js
@@ -1,9 +1,16 @@
-import React, {Component} from 'react';
+import React, {Component, createContext} from 'react';
+
+let {Provider,Consumer} = createContext();
 
 class Child extends Component {
     render(){
         return (
-            <strong>这是祖先传下来的宝贝: {this.props.info}</strong>
+                <Consumer>
+                    {(context)=>{
+                        console.log(context);
+                        return <strong>这是祖先传下来的宝贝:{context.info} </strong>
+                    }}
+                 </Consumer>
         )
     }
 }
@@ -11,7 +18,7 @@ class Child extends Component {
 class Parent extends Component {
     render(){
         return (<p>
-            <Child info={this.props.info}/>
+            <Child/>
         </p>);
     }
 }
@@ -19,9 +26,9 @@ class Parent extends Component {
 class Context extends Component {
    render(){
        return <div>
-           <Parent
-               info ={"今天天气不错！"}
-           />
+           <Provider value={{info :"今天天气不错"}}>
+               <Parent />
+           </Provider>
        </div>
    }
 }
