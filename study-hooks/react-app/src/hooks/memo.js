9d7a970b068735feb2271ad2a24269eb9a353dd3
Mark Liu <ldtheended@163.com>
2020-07-10 12:27:01 +0800

    as0.07-2-example07-2（useMemo 其与useEffect区别）

--- study-hooks/react-app/src/hooks/memo.js
+++ study-hooks/react-app/src/hooks/memo.js
@@ -1,12 +1,17 @@
-import React, { useState, useMemo} from 'react';
+import React, {useState, useMemo, useEffect} from 'react';
 
 function Memo() {
     const [name,setName] = useState("leo");
     const [age,setAge] = useState(10);
-    let age2 = (()=>{
+    let age2 = useMemo(()=>{
         console.log(1);
         return age < 18?"未成年":"成年人";
-    })();
+    },[age]);
+
+    useEffect(()=>{
+        console.log("更新完成之后！")
+    },[age])
+    console.log("开始更新了");
     return (<div>
         姓名:{name},<br />
         年龄:{age},<br />
