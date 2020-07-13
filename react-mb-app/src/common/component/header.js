28acc77a3c33480dc0c57003a25a780e90473cbc
Mark Liu <ldtheended@163.com>
2020-07-14 00:06:03 +0800

    根据路由信息判断显示菜单按钮和返回按钮。

--- react-mb-app/src/common/component/header.js
+++ react-mb-app/src/common/component/header.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {useBack} from "../hook/index";
+import {Link,withRouter} from "react-router-dom";
 
 function Header(props){
+    const back = useBack(props.history);
+    const path = props.location.pathname;
     return (
         <header id="header">
             <nav className="menu">
-
+                {/* 返回按钮 */}
+                {path === "/login" ? <a
+                    className="header-btn-left iconfont icon-back"
+                    onClick={()=>{
+                        back();
+                    }}
+                ></a> : <a
+                    className="header-btn-left iconfont icon-hycaidan"
+                    onClick={()=>{
+                    }}
+                ></a>}
             </nav>
             <h1 className="logo">miaov.com</h1>
             <Link className="user" to="/login"/>
         </header>
     );
 } 
-export default Header;
+export default withRouter(Header);
