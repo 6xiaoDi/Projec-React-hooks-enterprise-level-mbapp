b07691cf4b617a52d6cec47b8bb3bd42793a695f
Mark Liu <ldtheended@163.com>
2020-07-14 18:01:13 +0800

    到了登录页面，不应该显示登录头像了。

--- react-mb-app/src/common/component/header.js
+++ react-mb-app/src/common/component/header.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {useBack} from "../hook/index";
 import {Link,withRouter} from "react-router-dom";
 
+
+function getUser(path){
+    return path === "/login" ? "" : <Link className="user" to="/login"/>
+}
+
 function Header(props){
     const back = useBack(props.history);
     const path = props.location.pathname;
@@ -9,19 +14,24 @@ function Header(props){
         <header id="header">
             <nav className="menu">
                 {/* 返回按钮 */}
-                {path === "/login" ? <a
+                {path === "/login" ?
+                    <a
                     className="header-btn-left iconfont icon-back"
                     onClick={()=>{
                         back();
                     }}
-                ></a> : <a
+                    >
+                    </a>
+                    :
+                    <a
                     className="header-btn-left iconfont icon-hycaidan"
                     onClick={()=>{
                     }}
-                ></a>}
+                    >
+                    </a>}
             </nav>
             <h1 className="logo">miaov.com</h1>
-            <Link className="user" to="/login"/>
+            {getUser(path)}
         </header>
     );
 } 
