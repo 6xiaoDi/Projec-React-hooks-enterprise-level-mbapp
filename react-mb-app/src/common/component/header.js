d0b72afd172153e01fa4bd742314942f0d3b2cfa
Mark Liu <ldtheended@163.com>
2020-07-14 21:41:57 +0800

    完成整个登录功能，登录最后一个功能，登录状态下点击用户弹出退出功能

--- react-mb-app/src/common/component/header.js
+++ react-mb-app/src/common/component/header.js
@@ -1,32 +1,46 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import {useBack} from "../hook/index";
 import {Link,withRouter} from "react-router-dom";
 import {connect} from "react-redux";
 import isLogin from "../../store/action/isLogin";
+import logout from "../../store/action/logout";
 
-function getUser(path, user){
-    if(path === "/login"){
-        return ""
-    }
-    // 判断是否登录了
-    if(user){
-        return (<span className="header-btn-right">
-                <span
-                    className="header-user"
-                >{user}</span>
-            </span>);
-    }
-    return <Link className="user" to="/login" />;
-}
+
 
 function Header(props){
     const back = useBack(props.history);
     const path = props.location.pathname;
     const {user} =  props;
+    const [isBtnShow,setBtnShow] = useState(false);
     // 第二个参数传空数组即副作用钩子意思是在组件挂载后执行。
     useEffect(()=>{
         props.dispatch(isLogin());
     },[]);
+
+    function getUser(){
+        if(path === "/login"){
+            return ""
+        }
+        // 判断是否登录了
+        if(user){
+            return (<span className="header-btn-right">
+                <span
+                    className="header-user"
+                >{user}</span>
+                <span
+                    className="header-logout-btn"
+                    style={{
+                        display:isBtnShow?"block":"none"
+                    }}
+                    onClick={()=>{
+                        dispatch(logout());
+                    }}
+                >退出</span>
+            </span>);
+        }
+        return <Link className="user" to="/login" />;
+    }
+
     return (
         <header id="header">
             <nav className="menu">
@@ -48,7 +62,7 @@ function Header(props){
                     </a>}
             </nav>
             <h1 className="logo">miaov.com</h1>
-            {getUser(path,user)}
+            {getUser()}
         </header>
     );
 }
---
+++ react-mb-app/src/store/action/logout.js
@@ -0,0 +1,12 @@
+import HTTP from "./http";
+export default function loginOut(data){
+    return function(dispatch){
+       return HTTP.post("/user/logout").then(res=>{
+            if(res.data.code === 0){
+                dispatch({
+                    type: "LOGOUT"
+                });
+            }
+        })
+    }
+}
