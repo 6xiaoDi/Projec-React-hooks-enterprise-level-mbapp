0dc9a58efbb698059a15f20699c807ba036bde94
Mark Liu <ldtheended@163.com>
2020-07-18 21:55:26 +0800

    优化成局部刷新

--- react-mb-app/src/common/component/menu.js
+++ react-mb-app/src/common/component/menu.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 export default function Menu(){
     return (
         <nav id="menu">
-            <a className="iconfont icon-home" href="/">首页</a>
-            <a className="iconfont icon-kecheng" href="/course">课程安排</a>
-            <a className="iconfont icon-peixunjiangshi" href="/lecturer">讲师团队</a>
+            <NavLink className="iconfont icon-home" to="/">首页</NavLink>
+            <NavLink className="iconfont icon-kecheng" to="/course">课程安排</NavLink>
+            <NavLink className="iconfont icon-peixunjiangshi" to="/lecturer">讲师团队</NavLink>
         </nav>
     );
-} 
+}
