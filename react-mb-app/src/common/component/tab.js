657b4ce47ed8c78ef693e281553bf66348f87dfa
Mark Liu <ldtheended@163.com>
2020-07-20 22:21:04 +0800

    初步实现导航点随图片切换

--- react-mb-app/src/common/component/tab.js
+++ react-mb-app/src/common/component/tab.js
@@ -4,16 +4,22 @@ import BScroll from "better-scroll";
 export default function Tab(props){
     let {data,render} = props;
     let bannerWrap = useRef(null); // 幻灯片外框
+    let [now,setNow] = useState(0); // 代表当前被选中的是第几张图
     let bScroll = null;
     useEffect(()=>{
         bScroll = new BScroll(bannerWrap.current,{
             scrollX: true,
             scrollY: false,
             eventPassthrough: "vertical", // 保留纵向滑屏
+            momentum: false, // 关闭轻浮
             snap:{
                 loop: true // 无缝滑屏
             }
-        })
+        });
+        bScroll.on("scrollEnd",()=>{
+            // console.log(bScroll.getCurrentPage());
+            setNow(bScroll.getCurrentPage().pageX);
+        });
     },[]);
     return (<div className="banner">
         <div className="banner_img" ref={bannerWrap}>
@@ -26,8 +32,10 @@ export default function Tab(props){
         {
             data.length < 1 ?"":(<ul className="banner_nav">
                 {
-                    data.map((item,index)=><li key={index}>
-
+                    data.map((item,index)=><li
+                        key={index}
+                        className={index===now ? "active" : ""}
+                    >
                     </li>)
                 }
             </ul>)
