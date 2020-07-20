03744af7631166be2d3353c9a4d9b608ec3d35f0
Mark Liu <ldtheended@163.com>
2020-07-20 20:34:04 +0800

    初步完成滑屏配置

--- react-mb-app/src/common/component/tab.js
+++ react-mb-app/src/common/component/tab.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React,{useEffect,useRef,useState} from 'react';
 import BScroll from "better-scroll";
 
 export default function Tab(props){
     let {data,render} = props;
+    let bannerWrap = useRef(null); // 幻灯片外框
+    let bScroll = null;
+    useEffect(()=>{
+        bScroll = new BScroll(bannerWrap.current,{
+            scrollX: true,
+            scrollY: false,
+            eventPassthrough: "vertical", // 保留纵向滑屏
+            snap:{
+                loop: true // 无缝滑屏
+            }
+        })
+    },[]);
     return (<div className="banner">
-        <div className="banner_img">
+        <div className="banner_img" ref={bannerWrap}>
             <ul className="banner_list clearfix">
                 {
                     data.map((item,index)=><li key={index}>{render(item)}</li>)
