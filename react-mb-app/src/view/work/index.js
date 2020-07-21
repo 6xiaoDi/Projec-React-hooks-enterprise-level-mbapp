811e414d5235b4d0caf238c178246ea795d067e3
Mark Liu <ldtheended@163.com>
2020-07-21 17:14:11 +0800

    作品详情框子

--- react-mb-app/src/view/work/index.js
+++ react-mb-app/src/view/work/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Frame from "../../common/component/frame";
+import "../../common/css/coursev.css"
 
-function Work() {
-    return (<h1>
-        首页
-    </h1>)
+function Work(props) {
+    return (
+        // 不能直接写在框架里，有些东西在框架外，如footer
+        <div>
+            <Frame/>
+            <footer className="miiapv_footer">
+                回复本帖
+            </footer>
+        </div>
+    );
 }
 
 export default Work;
