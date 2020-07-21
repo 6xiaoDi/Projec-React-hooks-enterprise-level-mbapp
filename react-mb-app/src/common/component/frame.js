26075fec5361d0bc43cfeee78d68ed9f339dd095
Mark Liu <ldtheended@163.com>
2020-07-21 13:38:04 +0800

    完成课程页面视图并禁止回弹

--- react-mb-app/src/common/component/frame.js
+++ react-mb-app/src/common/component/frame.js
@@ -9,7 +9,7 @@ export default function Frame(props){
     const innerH = useInnerHeight();
     const wrap = useRef(null);
     let pageScroll = null;
-    let {pullUp,getData} = props;
+    let {pullUp,getData,bounce} = props;
     function changeShow() {
         setShowMenu(!showMenu);
     }
@@ -23,7 +23,8 @@ export default function Frame(props){
             preventDefaultException:{
                 tagName: /^(INPUT|TEXTAREA|BUTTON|SELECT|A)$/ // 注意大写
             },
-            pullUpLoad: pullUp?{threshold:200}:false  // 是否支持上滑加载
+            pullUpLoad: pullUp?{threshold:200}:false,  // 是否支持上滑加载
+            bounce
         });
         pageScroll.on("pullingUp",()=>{
             console.log("上滑加载更多");
